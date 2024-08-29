a15c7f4c83f4b8bebfbfef340b97b485044bd476
Srikanth1804 <130439363+Srikanth1804@users.noreply.github.com>
2024-08-29 17:07:25 +0530

    Add files via upload

---
+++ CSS/index.js
@@ -0,0 +1,50 @@
+let A=["Sri","kanth","Krish"];
+
+
+// console.log(A);
+
+//array:
+
+let H=[2,3,4,5];
+let E=[6,7,8,9]
+// // console.log(typeof H.toString());
+// console.log(H.push(7));
+// console.log(H);
+// console.log(H.pop());
+// console.log(H);
+// console.log(H.shift());
+// console.log(H);
+// console.log(H.unshift(0));
+// console.log(H);
+
+// console.log(delete H[2]);
+ console.log(H.concat(E));
+
+ //console.log(H.copyWithin(1,3));
+let DuplicateData = H.copyWithin(1,2);
+
+console.log(DuplicateData);
+
+
+let IndiaState = [[1,2,3],[4,5,6],[7,8,9]];
+console.log(IndiaState[1][2]);
+let WholeArray = IndiaState.flat()
+console.log(WholeArray);
+
+let Data=["A","B","C","D",];
+console.log(Data.splice(3,0,"H"));
+console.log(Data);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
